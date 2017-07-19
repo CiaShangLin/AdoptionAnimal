e6e113ebd89fcf833a345be05a249d703d392ce0
CiaShangLin <nick034po@yahoo.com.tw>
2017-07-19 19:15:51 +0800

    adapter修改

--- app/src/main/java/fcu/shang/adoptionanimal/MainActivity.java
+++ app/src/main/java/fcu/shang/adoptionanimal/MainActivity.java
@@ -151,11 +151,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.menu_search) {
             // Handle the camera action
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.menu_doctors) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.menu_lost) {
 
         }
 
