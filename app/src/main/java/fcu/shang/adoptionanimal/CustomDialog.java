342ab189bb51e3fb01984cd6cc13fd5f73873dcf
CiaShangLin <nick034po@yahoo.com.tw>
2017-07-31 00:27:39 +0800

    ｃｕｓｔｏｍ　ｄｉａｌｏｇ

---
+++ app/src/main/java/fcu/shang/adoptionanimal/CustomDialog.java
@@ -0,0 +1,16 @@
+package fcu.shang.adoptionanimal;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by SERS on 2017/7/30.
+ */
+
+public class CustomDialog extends Dialog{
+
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+    }
+}
